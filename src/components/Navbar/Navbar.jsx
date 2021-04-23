27ca538ac4e325f0fed5463570881da3f41256eb
Denzale Houston <denzalethouston@gmail.com>
2021-04-23 11:23:06 -0700

    Payment Form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,24 +3,28 @@ import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/cor
 import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/ApeShit.png'
 import useStyles from './styles'
+import { Link, useLocation } from 'react-router-dom'
 const Navbar = ({ totalItems }) => {
-    const classes = useStyles()
+    const classes = useStyles();
+    const location = useLocation();
+  
     return (
         <>
-        <AppBar position="fixed" className={classes.appBar} color="inherit" > 
-        <Toolbar>
-            <Typography variant="h6" className={classes.title} color="inherit">
-                <img src = {logo} alt="Ape.js" height="25px" className={classes.image} />
+            <AppBar position="fixed" className={classes.appBar} color="inherit" >
+                <Toolbar>
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="Ape.js" height="25px" className={classes.image} />
                 Ape Shit
             </Typography>
-            <div className={classes.grow} />
-            <div className={classes.button}>
-                <IconButton aria-label ="Show cart items" color="inherit">
-                <Badge badgeContent={totalItems} color="secondary"></Badge>
-                <ShoppingCart></ShoppingCart>
-                </IconButton>
-            </div>
-            </Toolbar>
+                    <div className={classes.grow} />
+                    {location.pathname === '/' && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary"></Badge>
+                            <ShoppingCart></ShoppingCart>
+                        </IconButton>
+                    </div>)}
+                </Toolbar>
             </AppBar>
         </>
     )
