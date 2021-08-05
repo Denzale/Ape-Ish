14d6101856b676d6264c2129b15c2e4460d37677
Denzale <denzalethouston@gmail.com>
2021-08-04 19:34:26 -0700

    sub division fixed

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { useFormContext, Controller } from 'react-hook-form';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import { TextField, Grid } from '@material-ui/core';
 
 function FormInput({ name, label, required }) {
-    const { control } = useFormContext();
-    const isError = false;
-    const errorMessage = '';
+  const { control } = useFormContext();
+  const isError = false;
 
-    return (
-        <Grid item xs={12} sm={6}>
-            <Controller
-                as={TextField}
-                name={name}
-                control={control}
-                defaultValue=""
-                label={label}
-                fullWidth
-                required={required}
-                error={isError}
-                helperText={errorMessage}
-            />
-        </Grid>
-    );
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        as={TextField}
+        name={name}
+        control={control}
+        label={label}
+        fullWidth
+        required={required}
+        error={isError}
+      />
+    </Grid>
+  );
 }
 
 export default FormInput;
